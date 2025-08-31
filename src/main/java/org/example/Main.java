d16a0e4916d5ec8d98263867800c23ceea4bd334
Deevesh Beegun <35701065+DeeveshBeegun@users.noreply.github.com>
2025-08-31 21:24:30 +0400

    Downgrade vosk library

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,20 +7,19 @@ import java.io.InputStream;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
-
 import org.vosk.LibVosk;
 import org.vosk.LogLevel;
 import org.vosk.Model;
-import org.vosk.Recognizer; 
+import org.vosk.Recognizer;
 
 
 public class Main {
     public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
         LibVosk.setLogLevel(LogLevel.DEBUG);
 
-        try (Model model = new Model("model");
+        try (Model model = new Model("/Users/deeveshbeegun/Developer/Projects/s2t/model/vosk-model-small-en-us-0.15");
                     InputStream inputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(
-                        new FileInputStream("")));
+                        new FileInputStream("/Users/deeveshbeegun/Developer/Projects/s2t/test.wav")));
                         Recognizer recognizer = new Recognizer(model, 16000);) {
 
                     int nbytes; 
